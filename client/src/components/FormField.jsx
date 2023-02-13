150f4b45be9eb0d6bb12fe5e50c85e3759dfaf32
Lakshya Purwar <purwarlakshya@gmail.com>
2023-02-13 17:30:09 +0530

    Added the static create post page

--- client/src/components/FormField.jsx
+++ client/src/components/FormField.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
 
-const FormField = () => {
+const FormField = ({ label, type, name, placeholder, value, handleChange, isSurpriseMe, handleSurpriseMe }) => {
   return (
-    <div>FormField</div>
+    <div>
+
+      <div className='flex item-center gap-2 mb-2'>
+        <label htmlFor={name} className='block text-sm font-medium text-gray-900'>
+          {label}
+        </label>
+        {isSurpriseMe && (
+          <button
+            type='button'
+            onClick={handleSurpriseMe}
+            className='font-semibold text-xs bg-[#ECECF1] py-1 px-2 rounded-[5px] text-black' >
+            Surprise Me
+          </button>)}
+      </div>
+
+      <input type={type}
+        id={name}
+        name={name}
+        placeholder={placeholder}
+        value={value}
+        onChange={handleChange}
+        required
+        className='bg-gray-50 border border-gray-300 text-gray-600 text-sm font-[600] rounded-lg focus:ring-[#4649ff] focus:ring-1 focus:border-[#4649ff] outline-none block w-full p-3'
+      />
+
+      
+
+
+
+
+    </div>
   )
 }
 
